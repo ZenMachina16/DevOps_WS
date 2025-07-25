a9d71e56c902340244969e7588ad9f32d0ff068a
Vishwajeet <vpanaskar9@gmail.com>
2025-07-25 11:58:29 +0530

    Update deploy workflow and Flask app

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def health_check():
 
 @app.route('/api/books', methods=['GET'])
 def get_all_books():
-    """Get all the books"""
+    """Get all the books from the library"""
     return jsonify({
         "books": books,
         "count": len(books)
